82e762da4426ed2c88428f5580f86105ae92ff34
Juliane Blier <blier.juliane@gmail.com>
2017-03-15 16:20:37 +0100

    Déplacement logique des items dans App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import Items from './Items';
+import ItemInput from './ItemInput';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      items : [
+        {
+          id : 1,
+          content : 'Passer l\'aspirateur'
+        },
+        {
+          id : 2,
+          content : 'Laver la vaisselle'
+        }
+      ]
+    };
+  }
+
+  addNewTask = (task) => {
+    console.log(task);
+  }
+
+
   render() {
     return (
       <div>
-        <Items />
+        <ItemInput onNewTask={ this.addNewTask } />
+        <Items tasks={this.state.items}/>
       </div>
     );
   }
--- src/Items.js
+++ src/Items.js
@@ -1,34 +1,13 @@
 import React from 'react';
-import ItemInput from './ItemInput';
 
 class Items extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      items : [
-        {
-          id : 1,
-          content : 'Passer l\'aspirateur'
-        },
-        {
-          id : 2,
-          content : 'Laver la vaisselle'
-        }
-      ]
-    }
-  }
-
-  addNewTask = (task) => {
-    console.log(task);
-  }
 
   render() {
-    const listItems = this.state.items.map((item) => {
+    const listItems = this.props.tasks.map((item) => {
       return <li key={ item.id.toString() }> { item.content } </li>
     });
     return (
       <div>
-        <ItemInput onNewTask={ this.addNewTask } />
         <ul>{ listItems }</ul>
       </div>
     )
