6fcbaaf2787354f095843a56e2f821acdbdc11c8
Juliane Blier <blier.juliane@gmail.com>
2017-03-15 14:01:09 +0100

    Affichage de la list en state

---
+++ src/Items.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Items extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      items : [
+        {
+          id : 1,
+          content : 'Passer l\'aspirateur'
+        },
+        {
+          id : 2,
+          content : 'Laver la vaisselle'
+        }
+      ]
+    }
+  }
+  render() {
+    const listItems = this.state.items.map((item) => {
+      return <li key= { item.id.toString() } > { item.content } </li>
+    });
+    return (
+      <ul>{ listItems }</ul>
+    )
+  }
+}
+
+
+export default Items;
